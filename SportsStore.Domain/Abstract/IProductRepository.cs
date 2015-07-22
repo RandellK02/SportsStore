893e4d36018a20fd886ef183afe4da062fa91442
RandellK02 <RandellK02@gmail.com>
2015-07-21 21:54:35 -0700

    update

--- SportsStore.Domain/Abstract/IProductRepository.cs
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SportsStore.Domain.Abstract
 {
-    interface IProductsRepository
+    public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
     }
