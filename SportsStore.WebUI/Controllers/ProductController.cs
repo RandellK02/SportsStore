ddbfff575eb0485833554119786536a5595815db
RandellK02 <RandellK02@gmail.com>
2015-07-22 18:08:20 -0700

    update ch 8

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,21 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepo)
         {
             this.repository = productRepo;
         }
 
-        public ViewResult List()
+        public ViewResult List(String category, int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                                            .Where(p => category == null || p.Category == category)
+                                            .OrderBy( p => p.ProductID )
+                                            .Skip( (page - 1) * PageSize )
+                                            .Take( PageSize ),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+                CurrentCategory = category
+            };
+            return View( model );
         }
 
     }
