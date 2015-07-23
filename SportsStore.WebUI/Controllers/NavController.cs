b1c44dac69bb764607b3d4aaf19461b15028c412
RandellK02 <RandellK02@gmail.com>
2015-07-22 20:40:57 -0700

    Pre Shopping Cart Ch 8

---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        //
+        // GET: /Nav/
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<String> categories = repository.Products
+                                                        .Select(x => x.Category)
+                                                        .Distinct()
+                                                        .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+
+    }
+}
